b840ed1c163299e7a1d37ab666963609b8fd391a
Karolina Motužytė <karolina.motuzyte02@gmail.com>
2023-06-27 17:53:49 +0200

    [ADDED] Implemented game object class

--- Src/GameObject.cpp
+++ Src/GameObject.cpp
@@ -20,9 +20,7 @@ void GameObject::Update(uint currentFrame)
 	static auto startTime = std::chrono::high_resolution_clock::now();
 	auto currentTime = std::chrono::high_resolution_clock::now();
 	float time = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();
-	//mat4 rotation = glm::rotate(glm::mat4(1.0f), glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-	//rotation = glm::rotate(rotation, time * glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-	//transform.rotation = angleAxis(time * glm::radians(90.0f), Common::GetWorldUp()) * defaultTransform.rotation;
+
 	transform.rotation = glm::angleAxis(glm::radians(90.f), Common::GetWorldUp()) * transform.rotation;
 
 	model->UpdateModelMatrix(transform, currentFrame);
