61708af5b24539e3468daa7634dcdb8a9111e44d
SantiagoBerna <santibberna@gmail.com>
2023-08-25 16:17:41 +0100

    Removed unnecessary compile script

--- Shaders/compile.bat
+++ Shaders/compile.bat
@@ -1,5 +1,5 @@
-D:/Vulkan/Bin/glslc.exe shader.vert -o defaultVert.spv
-D:/Vulkan/Bin/glslc.exe shader.frag -o defaultFrag.spv
-D:/Vulkan/Bin/glslc.exe box.vert -o boxVert.spv
-D:/Vulkan/Bin/glslc.exe box.frag -o boxFrag.spv
+C:/VulkanSDK/1.3.243.0/Bin/glslc.exe shader.vert -o target/defaultVert.spv
+C:/VulkanSDK/1.3.243.0/Bin/glslc.exe shader.frag -o target/defaultFrag.spv
+C:/VulkanSDK/1.3.243.0/Bin/glslc.exe box.vert -o target/boxVert.spv
+C:/VulkanSDK/1.3.243.0/Bin/glslc.exe box.frag -o target/boxFrag.spv
 pause
